fa90ceaf0b4eb2b7dff80db155f4454cbfd35040
lance031297 <44550756+lance031297@users.noreply.github.com>
2019-01-13 22:46:35 +0000

    Update

--- test_Person_Class.py
+++ test_Person_Class.py
@@ -13,9 +13,23 @@ class TestPerson_class(unittest.TestCase):
     	self.assertTrue(points(1, 2, 3))
     	self.assertRaise(points('e', ' ')ValueError)
 
+    def test_calculateScore(self):
+    	self.assertType(int)
+
+    def test_votesForMe(self, member, points)
+	    votes = dict()
+		votes.[member] = point
+		self.assertEqual(team[0],point)
+		votes.count = 5
+		self.assertRaises(IndexError,votes[point])
+
+
     def test_addVotes(self, member, points)
 	    votes = dict()
 			  	votes.[member] = point
 			    self.assertEqual(team[0],point)
 			    votes.count = 5
 			    self.assertRaises(IndexError,votes[point])
+
+if __name__ == '__main__':
+    unittest.main()
