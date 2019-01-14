3cfe8a3498397935fe84a4c88e3c8289dcd24716
lance031297 <44550756+lance031297@users.noreply.github.com>
2019-01-14 00:23:08 +0000

    Update

--- test_main.py
+++ test_main.py
@@ -49,4 +49,24 @@ class TestDeliverable_2(unittest.TestCase):
 		    self.assertEqual(team[0],point)
 		    team.count = 5
 		    self.assertRaises(IndexError,tempDict[point])
-        
+
+    def test_readFile(self, first, second, msg=None):
+        """Assert that two multi-line strings are equal.
+
+        If they aren't, show a nice diff.
+
+        """
+        self.assertTrue(isinstance(first, str),
+                'First argument is not a string')
+        self.assertTrue(isinstance(second, str),
+                'Second argument is not a string')
+
+        if first != second:
+            message = ''.join(difflib.ndiff(first.splitlines(True),
+                                                second.splitlines(True)))
+            if msg:
+                message += " : " + msg
+            self.fail("Multi-line strings are unequal:\n" + message)
+
+if __name__ == '__main__':
+    unittest.main()
